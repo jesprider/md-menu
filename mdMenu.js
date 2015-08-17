5bc92ba039626bcfbca7216145261a57491f79fc
Roman Alekseev <roman.alekseev@corp.mail.ru>
2015-08-17 19:29:05 +0300

    Add placeholder.

--- mdMenu.js
+++ mdMenu.js
@@ -1,16 +1,25 @@
 var fs = require('fs');
 
 var config = {
-    target: 'styleguide.md',
+    target: 'README.md',
     cascade: true,
-    firstLevel: 2
+    firstLevel: 2,
+    menuTitle: '## Table of Contents',
+    placeholder: '<!--[mdMenu]-->'
 };
 
 fs.readFile(config.target, function(err, data) {
     if (err) throw err;
 
     var regexp = /#{1,6}\s[^\r\n|\r|\n]+/g;
-    var headersArr = data.toString().match(regexp);
+    var source = data.toString();
+    var headersArr = source.match(regexp);
+    var res = '';
+
+    if (headersArr === null || !headersArr.length) {
+        console.log('No headers were found.');
+        return;
+    }
 
     // todo: optimize
     headersArr = headersArr.map(function(header) {
@@ -20,16 +29,23 @@ fs.readFile(config.target, function(err, data) {
             // Detect level of header
             var level = (header.match(/#/g) || []).length;
             // Save tabs if needed
-            tabs = new Array(level - config.firstLevel + 1).join('\t');
+            tabs = new Array(level - config.firstLevel + 1).join('\t') + '* ';
         }
 
         // Remove unnecessary symbols (#) and trim the string.
         header = header.replace(/#{1,6}\s/g, '').trim();
 
-        return tabs + '* [' + header + '](#' + header.replace(/&|\//g, '').replace(/\s/g, '-').toLowerCase() + ')';
+        return tabs + '[' + header + '](#' + header.replace(/&|\//g, '').replace(/\s/g, '-').toLowerCase() + ')';
     });
 
-    fs.writeFile('test.md', headersArr.join('\r\n'), function (err) {
+    if (source.indexOf(config.placeholder) !== -1) {
+        res = source.replace(config.placeholder, config.menuTitle + '\r\n' + headersArr.join('\r\n'));
+    } else {
+        res = config.menuTitle + '\r\n' + headersArr.join('\r\n') + '\r\n\r\n' + source;
+    }
+
+
+    fs.writeFile('content.md', res, function (err) {
         if (err) throw err;
 
         console.log('It\'s saved!');
